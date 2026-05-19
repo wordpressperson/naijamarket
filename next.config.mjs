813378f0fef65f3ce60df41c98c696be45d2aa49
Ayokunle <therealayokunle@gmail.com>
2026-05-19 18:56:28 +0100

    fixed module error

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,18 @@ const nextConfig = {
     };
     return config;
   },
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': [
+        'node_modules/viem',
+        'node_modules/wagmi',
+        'node_modules/@rainbow-me/rainbowkit',
+        'node_modules/framer-motion',
+        'node_modules/@swc/core-linux-x64-gnu',
+        'node_modules/@swc/core-linux-x64-musl',
+      ],
+    },
+  },
 };
 
 export default nextConfig;
